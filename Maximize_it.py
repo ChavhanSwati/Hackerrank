8a082e1f43107af09b15152fd6784df59dce1078
Swati Chavhan <60737245+ChavhanSwati@users.noreply.github.com>
2021-08-20 16:44:25 +0530

    Add files via upload

---
+++ Maximize_it.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from itertools import product
+
+k, m = map(int, input().split())
+n = (list(map(int, input().split()))[1:] for _ in range(k))
+results = map(lambda x: sum(i**2 for i in x) % m, product(*n))
+print(max(results))
+
